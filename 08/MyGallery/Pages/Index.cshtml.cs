640c175224ccbbbc84f05662a0c8d081ff543e32
Krzysztof <krzysiek@chomicze.com>
2024-10-01 18:29:59 +0200

    feat: wip

---
+++ 08/MyGallery/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+namespace MyGallery.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly IHostingEnvironment _environment;
+    internal List<FileModel> Files { get; set; } = [];
+    private readonly ILogger<IndexModel> _logger;
+    private const string _path = "Images";
+
+    public IndexModel(IHostingEnvironment environment, ILogger<IndexModel> logger)
+    {
+        _environment = environment;
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        var filePaths = Directory.GetFiles(Path.Combine(_environment.WebRootPath, _path), "*.jpg");
+        Files = filePaths.Select(x => new FileModel
+            { FileName = Path.GetFileName(x), Url = _path + "/" + Path.GetFileName(x) }).ToList();
+    }
+}
+
+internal sealed class FileModel
+{
+    public string FileName { get; set; } = default!;
+    public string Url { get; set; } = default!;
+}
---
+++ 08/README.md
@@ -0,0 +1,8 @@
+# Praca domowa 8 – Wprowadzenie do obrazów i kontenerów Docker
+
+Termin: 3 paź, 23:59
+
+
+- Utwórz obraz Docker dla prostego serwera HTTP.
+- Uruchom kontener z utworzonym obrazem w Azure Container Instances.
+- Zarządzaj siecią i danymi w kontenerze.
